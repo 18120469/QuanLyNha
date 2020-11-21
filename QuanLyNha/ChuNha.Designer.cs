62a7a20414b00dbbf9e9a0e27c0a076e2ed55357
SƠN VÕ NGUYỄN HỒNG <18120538@student.hcmus.edu.vn>
2020-11-21 11:18:50 +0700

    Sơn

---
+++ QuanLyNha/ChuNha.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace QuanLyNha
+{
+    partial class ChuNha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabDangTin = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnDangTin = new System.Windows.Forms.Button();
+            this.gb_ttcoban = new System.Windows.Forms.GroupBox();
+            this.lbChuThich = new System.Windows.Forms.Label();
+            this.datetimeNgayHetHan = new System.Windows.Forms.DateTimePicker();
+            this.datetimeNgayDang = new System.Windows.Forms.DateTimePicker();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbNgayHetHan = new System.Windows.Forms.Label();
+            this.lbKhuVuc = new System.Windows.Forms.Label();
+            this.lbQuan = new System.Windows.Forms.Label();
+            this.lbNgayDang = new System.Windows.Forms.Label();
+            this.lbGia = new System.Windows.Forms.Label();
+            this.lbSoLuongPhong = new System.Windows.Forms.Label();
+            this.lbThanhPho = new System.Windows.Forms.Label();
+            this.lbDuong = new System.Windows.Forms.Label();
+            this.lbHinhThuc = new System.Windows.Forms.Label();
+            this.lb_tenTieuDe = new System.Windows.Forms.Label();
+            this.tabLSGD = new System.Windows.Forms.TabPage();
+            this.tabQLNhaDang = new System.Windows.Forms.TabPage();
+            this.tabDangTin.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.gb_ttcoban.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabDangTin
+            // 
+            this.tabDangTin.Controls.Add(this.tabPage1);
+            this.tabDangTin.Controls.Add(this.tabLSGD);
+            this.tabDangTin.Controls.Add(this.tabQLNhaDang);
+            this.tabDangTin.Location = new System.Drawing.Point(0, 7);
+            this.tabDangTin.Name = "tabDangTin";
+            this.tabDangTin.SelectedIndex = 0;
+            this.tabDangTin.Size = new System.Drawing.Size(800, 475);
+            this.tabDangTin.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnDangTin);
+            this.tabPage1.Controls.Add(this.gb_ttcoban);
+            this.tabPage1.Controls.Add(this.lb_tenTieuDe);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 446);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Đăng tin";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnDangTin
+            // 
+            this.btnDangTin.Location = new System.Drawing.Point(629, 391);
+            this.btnDangTin.Name = "btnDangTin";
+            this.btnDangTin.Size = new System.Drawing.Size(111, 37);
+            this.btnDangTin.TabIndex = 7;
+            this.btnDangTin.Text = "Đăng tin";
+            this.btnDangTin.UseVisualStyleBackColor = true;
+            // 
+            // gb_ttcoban
+            // 
+            this.gb_ttcoban.Controls.Add(this.lbChuThich);
+            this.gb_ttcoban.Controls.Add(this.datetimeNgayHetHan);
+            this.gb_ttcoban.Controls.Add(this.datetimeNgayDang);
+            this.gb_ttcoban.Controls.Add(this.textBox6);
+            this.gb_ttcoban.Controls.Add(this.textBox5);
+            this.gb_ttcoban.Controls.Add(this.textBox4);
+            this.gb_ttcoban.Controls.Add(this.textBox3);
+            this.gb_ttcoban.Controls.Add(this.textBox2);
+            this.gb_ttcoban.Controls.Add(this.textBox1);
+            this.gb_ttcoban.Controls.Add(this.comboBox1);
+            this.gb_ttcoban.Controls.Add(this.lbNgayHetHan);
+            this.gb_ttcoban.Controls.Add(this.lbKhuVuc);
+            this.gb_ttcoban.Controls.Add(this.lbQuan);
+            this.gb_ttcoban.Controls.Add(this.lbNgayDang);
+            this.gb_ttcoban.Controls.Add(this.lbGia);
+            this.gb_ttcoban.Controls.Add(this.lbSoLuongPhong);
+            this.gb_ttcoban.Controls.Add(this.lbThanhPho);
+            this.gb_ttcoban.Controls.Add(this.lbDuong);
+            this.gb_ttcoban.Controls.Add(this.lbHinhThuc);
+            this.gb_ttcoban.Location = new System.Drawing.Point(17, 54);
+            this.gb_ttcoban.Name = "gb_ttcoban";
+            this.gb_ttcoban.Size = new System.Drawing.Size(758, 374);
+            this.gb_ttcoban.TabIndex = 6;
+            this.gb_ttcoban.TabStop = false;
+            this.gb_ttcoban.Text = "Thông tin cơ bản";
+            // 
+            // lbChuThich
+            // 
+            this.lbChuThich.AutoSize = true;
+            this.lbChuThich.Location = new System.Drawing.Point(6, 283);
+            this.lbChuThich.Name = "lbChuThich";
+            this.lbChuThich.Size = new System.Drawing.Size(222, 34);
+            this.lbChuThich.TabIndex = 18;
+            this.lbChuThich.Text = "(*) Hình thức bán: Giá bán\r\n     Hình thức thuê: Giá thuê/tháng";
+            // 
+            // datetimeNgayHetHan
+            // 
+            this.datetimeNgayHetHan.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datetimeNgayHetHan.Location = new System.Drawing.Point(542, 237);
+            this.datetimeNgayHetHan.Name = "datetimeNgayHetHan";
+            this.datetimeNgayHetHan.Size = new System.Drawing.Size(110, 22);
+            this.datetimeNgayHetHan.TabIndex = 17;
+            // 
+            // datetimeNgayDang
+            // 
+            this.datetimeNgayDang.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datetimeNgayDang.Location = new System.Drawing.Point(126, 237);
+            this.datetimeNgayDang.Name = "datetimeNgayDang";
+            this.datetimeNgayDang.Size = new System.Drawing.Size(114, 22);
+            this.datetimeNgayDang.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(126, 196);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(163, 22);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(126, 153);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(76, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(542, 80);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(196, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(542, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(196, 22);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(126, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // lbNgayHetHan
+            // 
+            this.lbNgayHetHan.AutoSize = true;
+            this.lbNgayHetHan.Location = new System.Drawing.Point(430, 237);
+            this.lbNgayHetHan.Name = "lbNgayHetHan";
+            this.lbNgayHetHan.Size = new System.Drawing.Size(93, 17);
+            this.lbNgayHetHan.TabIndex = 8;
+            this.lbNgayHetHan.Text = "Ngày hết hạn";
+            // 
+            // lbKhuVuc
+            // 
+            this.lbKhuVuc.AutoSize = true;
+            this.lbKhuVuc.Location = new System.Drawing.Point(430, 80);
+            this.lbKhuVuc.Name = "lbKhuVuc";
+            this.lbKhuVuc.Size = new System.Drawing.Size(59, 17);
+            this.lbKhuVuc.TabIndex = 7;
+            this.lbKhuVuc.Text = "Khu vực";
+            // 
+            // lbQuan
+            // 
+            this.lbQuan.AutoSize = true;
+            this.lbQuan.Location = new System.Drawing.Point(430, 40);
+            this.lbQuan.Name = "lbQuan";
+            this.lbQuan.Size = new System.Drawing.Size(43, 17);
+            this.lbQuan.TabIndex = 6;
+            this.lbQuan.Text = "Quận";
+            // 
+            // lbNgayDang
+            // 
+            this.lbNgayDang.AutoSize = true;
+            this.lbNgayDang.Location = new System.Drawing.Point(6, 237);
+            this.lbNgayDang.Name = "lbNgayDang";
+            this.lbNgayDang.Size = new System.Drawing.Size(77, 17);
+            this.lbNgayDang.TabIndex = 5;
+            this.lbNgayDang.Text = "Ngày đăng";
+            // 
+            // lbGia
+            // 
+            this.lbGia.AutoSize = true;
+            this.lbGia.Location = new System.Drawing.Point(6, 199);
+            this.lbGia.Name = "lbGia";
+            this.lbGia.Size = new System.Drawing.Size(49, 17);
+            this.lbGia.TabIndex = 4;
+            this.lbGia.Text = "Giá (*)";
+            // 
+            // lbSoLuongPhong
+            // 
+            this.lbSoLuongPhong.AutoSize = true;
+            this.lbSoLuongPhong.Location = new System.Drawing.Point(6, 156);
+            this.lbSoLuongPhong.Name = "lbSoLuongPhong";
+            this.lbSoLuongPhong.Size = new System.Drawing.Size(108, 17);
+            this.lbSoLuongPhong.TabIndex = 3;
+            this.lbSoLuongPhong.Text = "Số lượng phòng";
+            // 
+            // lbThanhPho
+            // 
+            this.lbThanhPho.AutoSize = true;
+            this.lbThanhPho.Location = new System.Drawing.Point(6, 117);
+            this.lbThanhPho.Name = "lbThanhPho";
+            this.lbThanhPho.Size = new System.Drawing.Size(77, 17);
+            this.lbThanhPho.TabIndex = 2;
+            this.lbThanhPho.Text = "Thành phố";
+            // 
+            // lbDuong
+            // 
+            this.lbDuong.AutoSize = true;
+            this.lbDuong.Location = new System.Drawing.Point(6, 80);
+            this.lbDuong.Name = "lbDuong";
+            this.lbDuong.Size = new System.Drawing.Size(50, 17);
+            this.lbDuong.TabIndex = 1;
+            this.lbDuong.Text = "Đường";
+            // 
+            // lbHinhThuc
+            // 
+            this.lbHinhThuc.AutoSize = true;
+            this.lbHinhThuc.Location = new System.Drawing.Point(6, 40);
+            this.lbHinhThuc.Name = "lbHinhThuc";
+            this.lbHinhThuc.Size = new System.Drawing.Size(68, 17);
+            this.lbHinhThuc.TabIndex = 0;
+            this.lbHinhThuc.Text = "Hình thức";
+            // 
+            // lb_tenTieuDe
+            // 
+            this.lb_tenTieuDe.AutoSize = true;
+            this.lb_tenTieuDe.Location = new System.Drawing.Point(287, 23);
+            this.lb_tenTieuDe.Name = "lb_tenTieuDe";
+            this.lb_tenTieuDe.Size = new System.Drawing.Size(253, 17);
+            this.lb_tenTieuDe.TabIndex = 5;
+            this.lb_tenTieuDe.Text = "ĐĂNG TIN RAO BÁN, CHO THUÊ NHÀ";
+            // 
+            // tabLSGD
+            // 
+            this.tabLSGD.Location = new System.Drawing.Point(4, 25);
+            this.tabLSGD.Name = "tabLSGD";
+            this.tabLSGD.Padding = new System.Windows.Forms.Padding(3);
+            this.tabLSGD.Size = new System.Drawing.Size(792, 441);
+            this.tabLSGD.TabIndex = 1;
+            this.tabLSGD.Text = "Lịch sử giao dịch";
+            this.tabLSGD.UseVisualStyleBackColor = true;
+            // 
+            // tabQLNhaDang
+            // 
+            this.tabQLNhaDang.Location = new System.Drawing.Point(4, 25);
+            this.tabQLNhaDang.Name = "tabQLNhaDang";
+            this.tabQLNhaDang.Padding = new System.Windows.Forms.Padding(3);
+            this.tabQLNhaDang.Size = new System.Drawing.Size(792, 441);
+            this.tabQLNhaDang.TabIndex = 2;
+            this.tabQLNhaDang.Text = "Quản lý nhà đăng";
+            this.tabQLNhaDang.UseVisualStyleBackColor = true;
+            // 
+            // ChuNha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 467);
+            this.Controls.Add(this.tabDangTin);
+            this.Name = "ChuNha";
+            this.Text = "Form1";
+            this.tabDangTin.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.gb_ttcoban.ResumeLayout(false);
+            this.gb_ttcoban.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabDangTin;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnDangTin;
+        private System.Windows.Forms.GroupBox gb_ttcoban;
+        private System.Windows.Forms.Label lbChuThich;
+        private System.Windows.Forms.DateTimePicker datetimeNgayHetHan;
+        private System.Windows.Forms.DateTimePicker datetimeNgayDang;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbNgayHetHan;
+        private System.Windows.Forms.Label lbKhuVuc;
+        private System.Windows.Forms.Label lbQuan;
+        private System.Windows.Forms.Label lbNgayDang;
+        private System.Windows.Forms.Label lbGia;
+        private System.Windows.Forms.Label lbSoLuongPhong;
+        private System.Windows.Forms.Label lbThanhPho;
+        private System.Windows.Forms.Label lbDuong;
+        private System.Windows.Forms.Label lbHinhThuc;
+        private System.Windows.Forms.Label lb_tenTieuDe;
+        private System.Windows.Forms.TabPage tabLSGD;
+        private System.Windows.Forms.TabPage tabQLNhaDang;
+    }
+}
