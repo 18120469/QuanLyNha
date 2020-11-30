44e24196f41e697b672dfb66f43adf0b4bb2eb51
PHƯỢNG PHẠM THỊ BÍCH <18120518@student.hcmus.edu.vn>
2020-11-30 07:43:42 +0700

    p

--- QuanLyNha/KhachHang.Designer.cs
+++ QuanLyNha/KhachHang.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace QuanLyThueMuaNha
+﻿namespace QuanLyNha
 {
     partial class KhachHang
     {
--- QuanLyNha/KhachHang.cs
+++ QuanLyNha/KhachHang.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuanLyThueMuaNha
+namespace QuanLyNha
 {
     public partial class KhachHang : Form
     {
